d5e67b8fb27096219dc93b3dcb5bc77aa8cc4742
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-04-23 19:22:07 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
+import { StyleSheet, View } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const webViewRef = useRef(null);
+  const injectHideScript = (className1, className2) => {
+    const script = `
+      var elements = document.getElementsByClassName("${className1}");
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = 'none';
+      }
+      var elements = document.getElementsByClassName("${className2}");
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = 'none';
+      }
+    `;
+    webViewRef.current.injectJavaScript(script);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <WebView
+        style={styles.container}
+        ref={webViewRef}
+        onLoad={() => injectHideScript('site-header', 'elementor-button-wrapper')}
+        source={{ uri: "https://www.tonggiaophanhanoi.org/" }}
+        bounces={false}
+        allowsBackForwardNavigationGestures={true}
+      />
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // marginTop: -100
   },
 });
