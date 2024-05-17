5167b63960568aa990daa601a852b63c82e8c59d
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-05-17 10:23:46 +0700

    config build with http

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 const apiDev = "http://192.168.1.9:3000/api/v1"
 const apiProduct = "https://tntt-api.vercel.app/api/v1"
+const apiServer = "http://113.190.48.97:3000/api/v1"
 
 export default Utils = {
-    apiUrl: apiProduct,
+    apiUrl: apiDev,
 }
