a00e8624254ec110d9215ecf536078d297cb9b89
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-01-15 11:34:18 +0700

    update app

--- src/utils.js
+++ src/utils.js
@@ -3,5 +3,5 @@ const apiProduct = "https://tntt-api.vercel.app/api/v1"
 const apiServer = "http://113.190.48.97:3000/api/v1"
 
 export default Utils = {
-    apiUrl: apiDev,
+    apiUrl: apiProduct,
 }
